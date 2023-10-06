d922837c30361914e22197124f1b64a97fedc278
alvinchipmunk413 <alvin.manait413@gmail.com>
2023-10-06 22:50:29 +1300

    working

--- main.tf
+++ main.tf
@@ -1,5 +1,16 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example"
+  name     = "rg-palvs"
   location = "West Europe"
 }
 
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountpalvs2"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "client_secret" {
+    default="k~P8Q~0x8~dPaCXlSV4XQF4e0UlQLCViCTw7QcHn"
+}
+variable "client_id" {
+    default="60078c48-7b5f-4148-a943-f009a5c0956f"
+}
+variable "tenant_id" {
+    default="fbea7238-bf44-43b4-a22d-3de23799670b"
+}
+variable "subscription_id" {
+    default ="f5f8e3e5-3282-4032-a77f-70a7326de9e6"
+}
