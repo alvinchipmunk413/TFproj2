6a1e4d0b1693a03daf8e50a390e35c05948368bd
alvinchipmunk413 <alvin.manait413@gmail.com>
2023-10-10 00:57:55 +1300

    Working added https rule

--- main4.tf
+++ main4.tf
@@ -54,6 +54,17 @@ resource "azurerm_network_security_group" "my_terraform_nsg_v2" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "https"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 
 
